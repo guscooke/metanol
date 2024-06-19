e06f667b8521a2eb15f55603dd772685ffcd1db9
guscooke <guscooke14@gmail.com>
2024-06-19 09:30:53 -0300

    changes: minor

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -2,8 +2,9 @@ export default function Clients() {
     return (
 
         <div className="flex flex-col  min-h-screen bg-gradient-to-br from-slate-800 to-gray-900 sm:flex-row"  >
-            < h1 className="text-teal-100 text-5xl text-center" >
-                Penso em colocar aqui o historico do cliente
+
+            < h1 className=" text-teal-100 text-2xl align-middle " >
+                Historico dos clientes mais detalhado
             </h1 >
         </div >
     )
