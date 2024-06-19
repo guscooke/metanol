9fee42d19279024c3d28515c1dc697d2fca69e25
guscooke <guscooke14@gmail.com>
2024-06-19 08:37:42 -0300

    fix: client

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,10 +1,10 @@
 export default function Clients() {
     return (
 
-        <div className='bg-black' >
-            <h1 className="text-teal-900 text-5xl">
+        <div className="flex flex-col  min-h-screen bg-gradient-to-br from-slate-800 to-gray-900 sm:flex-row"  >
+            < h1 className="text-teal-100 text-5xl text-center" >
                 Penso em colocar aqui o historico do cliente
-            </h1>
-        </div>
+            </h1 >
+        </div >
     )
 }
